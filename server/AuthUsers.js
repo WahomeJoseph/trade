2e8c3c51a0b941713500df8928830e3b3539a9fb
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2024-11-12 10:30:02 +0300

    Update AuthUsers.js
    Authorizes users to login into the system whereas preventing unauthorized access
    
    Signed-off-by: Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>

--- server/AuthUsers.js
+++ server/AuthUsers.js
@@ -1,1 +1,35 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
+import asyncHandler from './asyncHandler.js'
 
+const authenticateUsers = asynchandler(async(req,res,next) => {
+  let token
+
+  // Read JWT tokens from jwt cookies
+  token = req.cookies.jwt
+
+ if (token) {
+   try {
+     const decoded = jwt.verify(token, process.env.JWT_SECRET)
+     req.user = await User.findById(decoded.userId).select('-password')
+     next()
+   } catch (error) {
+     res.status(401)
+     throw new Error('Authentication Required!')
+   }
+ } else {
+   res.status(401)
+   throw new Error ('Authentication Required!')
+ }
+})
+
+// Check if user id admin...only admin rights
+const authenticateAdmin = (req, res, next) => {
+  if (req.user && req.user isAdmin){
+    next()
+  } else {
+    res.status(401).json(message: 'Only Admins Allowed')
+  }
+}
+
+export {authenticateUsers, authenticateAdmin}
