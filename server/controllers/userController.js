83f49b5b0868c2de4116053266c412cf511390ac
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2024-11-12 11:21:12 +0300

    Update userController.js
    user controller....to be used with routes. create, login, assign cookies to users.... ADMIN CRUD ops
    
    Signed-off-by: Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,72 @@
 import User from "../models/userModel.js";
 import asyncHandler from '../middlewares/asyncHandler.js'
+import bcrypt from 'bcryptjs'
 
+// create a user account
 export const createUser = asyncHandler(async (req, res) => {
-    res.send('Hello There!')
+    const {username, email, password} = req.body
+
+    if(!username ||!email || !password) {
+        res.status(400).json(message: 'All inmputs are required!')
+    }
+
+    // add new users if not exist
+    const userExist = await User.findOne({email})
+    if (userExist)
+        res.status(400).json(message: 'User Already Exist!')
+
+    // creating strong password by encryptionto ensure security
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({username, email, password:hashedPassword})
+     try {
+         await newUser.save()
+         createToken(res, newUser._id)
+
+         res.status(200).json({
+             _id: newUser._id,
+             username: newUser.username,
+             email: newUser.email,
+             isAdmin: newUser.isAdmin,
+         })
+     } 
+    catch (error) {
+        res.status(400).json(message: 'User Not Found!')
+    }
+    
 })
+
+// login controller... users login into the system
+const loginUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    consiole.log(email)
+    console.log(password)
+
+    const exUser = await User.findOne ({email})
+    if (exUser) {
+        const isPasswordvalid = await bcrypt.compare(
+            password,
+            exUser.password
+        )
+
+        if (isPasswordValid){
+            createToken(res, exUser._id)
+
+            res.status(200).json({
+                _id: exUser._id,
+                username: exUser.username,
+                email: exUser.email,
+                isAdmin: exUser.isAdmin,
+            })
+            return
+            
+        }
+    }
+})
+
+
+// logout controller... tokens and cookies for timeout sessions
+
+
+
