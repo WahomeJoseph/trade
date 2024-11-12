0aed194f66b9a23d371e05ead476b5fce5690784
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2024-11-12 10:10:00 +0300

    Update userRoute.js
    Signed-off-by: Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,6 +2,7 @@ import express from 'express'
 import {createUser} from '../controllers/userController.js'
 const router = express.Router()
 
-router.route('/create').post(createUser)
+// end point to add new users
+router.route('/create').post(createUser).get(authenticate, authorizeAdmin, getAllUsers)
 
 export default router
