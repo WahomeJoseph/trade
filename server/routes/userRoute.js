6ac830e2e7fd781043a83d0c01cfa29a5de0411d
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2024-11-16 11:18:25 +0300

    Update userRoute.js
    route changes
    
    
    Signed-off-by: Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,23 +1,15 @@
 import express from 'express'
-<<<<<<< HEAD
-import {createUser} from '../controllers/userController.js'
-<<<<<<< HEAD
+import {createUser, loginUser, logoutCurrentUser, getCurrentUserProfile, updatecurrentUserProfile, getUser, deleteUser, updateUser} from '../controllers/userController.js'
+import {authenticateUsers, authenticateAdmin} from '../middlewares/AuthUsers.js'
 
 const router = express.Router()
-
 router.route('/').post(createUser)
-=======
-=======
-import {createUser, loginUser, logoutCurrentUser, getCurrentUserProfile, updatecurrentUserProfile, getUser, deleteUser, updateUser} from '../controllers/userController.js'
->>>>>>> 770c8dd450ffb3911f481d8058b6817de6658b51
-import {authenticateUsers, auhenticateAdmin} from '../middlewares/AuthUsers.js'
 const router = express.Router()
 
 // end point to add new users
 router.route('/create').post(createUser)
   // authorize users
   .get(authenticateUsers, authenticateAdmin, getAllUsers)
->>>>>>> 53ead0993cb34018e689ebaef64eabcf3e0097b0
 
 // user routes
 router.route('./login').post(loginUser)
