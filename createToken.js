de0c1a8155e047461ab30f863d4234a9c8693a38
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2024-11-27 10:40:24 +0300

    Update createToken.js
    Signed-off-by: Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>

--- createToken.js
+++ createToken.js
@@ -1,5 +1,13 @@
 import jwt from 'jsonwebtoken'
 
 const genToken = (res, userId) => {
-  const token = jwt.sign({userId}, )
+  const token = jwt.sign({userId}, process.env.JWT_SECRET_KEY, {
+    expiresIn: '30m'
+  })
+
+  // set jwt as a http only cookie
+  res.cookie(''jwt', token, {
+    httpOnly: true,
+    secure: process.env.JWT_NODE !== 'development',
+  })
 }
