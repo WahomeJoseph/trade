3eb7fdf8be3e43ca515e9fbd8efbcf6bbaf01fa0
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2024-11-28 14:22:04 +0300

    Update userRoute.js
    Signed-off-by: Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import {createUser, loginUser, logoutCurrentUser, getCurrentUserProfile, updateUser, getUser, deleteUser, updateUser} from '../controllers/userController.js'
-import {authenticateUsers, authenticateAdmin} from '../middlewares/AuthUsers.js'
+import {createUser, loginUser, logOutUser, getCurrentUserProfile, updateUser, getUser, deleteUser, updateUser} from '../controllers/userController.js'
+import {authUsers, authAdmin} from '../middlewares/AuthUsers.js'
 
 const router = express.Router()
 router.route('/').post(createUser)
@@ -9,17 +9,17 @@ router.route('/logOut').post(logOutUser) //logout route
 
 // end point to add new users  
 router.route('/create').post(createUser)
- .get(authenticateUsers, authenticateAdmin, getAllUsers)
+ .get(authUsers, authAdmin, getAllUsers)
 
 
 router.route('./profile')
-router.get(authenticateUsers, getCurrentUserprofile)
-router.put(authenticateUsers, getCurrentUserProfile)
+router.get(authUsers, getCurrentUserprofile)
+router.put(authUsers, getCurrentUserProfile)
 
 // ADMIN rights to manage users
 router.route('./:id')
-router.delete(authenticateUsers, authenticateAdmin, deleteUser)
-router.get(authenticateUsers, authenticateAdmin, getUsersById)
-router.get(authenticateUsers, authenticateAdmin, updateUser)
+router.delete(authUsers, authAdmin, deleteUser)
+router.get(authUsers, authAdmin, getUsersById)
+router.get(authUsers, authAdmin, updateUser)
 
 export default router
