bdd665853bf1f9ff3868aba87dbc3ae33cf601c0
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2024-12-19 12:17:02 +0000

    Api slicing to accommodate multiple endpoints!

--- client/src/redux/api/UsersApi.js
+++ client/src/redux/api/UsersApi.js
@@ -1,2 +1,14 @@
 import { apiSlice } from "./SliceApi";
 import { USERS_URL } from "../features/Constants";
+
+export const userApiSlice = apiSlice.injectEndpoint({
+    endpoints: (builder) => ({
+        login: builder.mutation ({
+            query: (data) => ({
+                url: `${USERS_URL}/auth`,
+                method: 'POST',
+                body: data,
+            })
+        })
+    })
+})
