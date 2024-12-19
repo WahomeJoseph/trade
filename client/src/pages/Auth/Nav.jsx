06b4c2c153f4beb0bbcc88aa09f144e5b1f59b8d
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2024-12-19 13:31:53 +0300

    Update Nav.jsx
    Signed-off-by: Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>

--- client/src/pages/Auth/Nav.jsx
+++ client/src/pages/Auth/Nav.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-
+import { AiOutlineHome, AiOutlineShopping, AiOutlineLogin, AiOutlineUserAdd, AiOutlineShoppingCart } from "react-icons/ai";
+import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./Nav.css";
+import { useSelector, useDispatch } from "react-redux";
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>Navigation</div>
   )
 }
 
