c8921299b104e295085843014d63af300d32252e
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2024-12-19 13:28:32 +0300

    Update main.jsx
    Signed-off-by: Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {Route, RouterProvider, createRouteForElement} from 'react-router'
-import {createBrowseRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
