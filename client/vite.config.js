1020046b8822243d53158b5e937be3b7b3900403
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-16 09:03:35 +0000

    progress

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, },
+  server: { 
+    proxy: {
+      '/api': 'http://localhost:5173'
+    }
+  }
 })
