903e22a0cf6861c4fda2d2f7d4bb2f38fd45d079
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-18 10:44:38 +0000

    progress

--- client/src/pages/User/Profile.jsx
+++ client/src/pages/User/Profile.jsx
@@ -23,12 +23,27 @@ export const Profile = () => {
 
     const dispatch = useDispatch()
 
+    const handleUpdate = async (e) => {
+        e.prevent()
+        if (password !== password) {
+            toast.error('Password must match!')
+        } else {
+            try {
+                const res = await updateProfile({_id: userInfo.id, username, email, password}).unwrap()
+                dispatch(setCredentials({...res}))
+                toast.success('Profile updated successfully!')
+            } catch (error) {
+                toast.error('Profile update failed!', error.message)
+            }
+        }
+    }
+
     return (
         <div className="container mx-auto p-4 mt-40">
             <div className="flex justify-center items-center md:flex md:space-x-4">
                 <h2 className="text-2xl font-semibold mb-4">Update Profile</h2>
 
-                <form action="">
+                <form onSubmit={handleUpdate} className="w-full md:w-1/2">
                     <div className="mb-4">
                         <label htmlFor="" className='block text-white mb-2'>Username</label>
                         <input type="text" value={username} onChange={(e) => setuserName(e.target.value)} placeholder='username' className='form-input p-4 rounded-sm w-full' />
@@ -48,11 +63,12 @@ export const Profile = () => {
 
                     <div className="flex justify-between items-center p-4">
                        <button className='bg-transparent text-white px-4 py-2 rounded-sm my-4 hover:bg-red-600'>Update</button>
-                       <Link to='/user-orders' className='bg-transparent text-white px-4 py-2 rounded-sm my-4 hover:bg-red-600'></Link>
+                       <Link to='/user-orders' className='bg-transparent text-white px-4 py-2 rounded-sm my-4 hover:bg-red-600'>My Orders</Link>
                     </div>
                 </form>
             </div>
             
+
         </div>
     )
 }
