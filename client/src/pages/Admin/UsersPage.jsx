61cbdb0660f10e38e0bb7f08ebfb65738a9dc9e7
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-18 11:16:32 +0000

    progress

--- client/src/pages/Admin/UsersPage.jsx
+++ client/src/pages/Admin/UsersPage.jsx
@@ -1,1 +1,12 @@
-import
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { toast } from 'react-toastify'
+import { FaTrashCan } from "react-icons/fa6";
+import { FiEdit3 } from "react-icons/fi";
+import { IoCheckmarkDoneSharp } from "react-icons/io5";
+
+export const UsersPage = () => {
+    return (
+        <div></div>
+    )
+}
