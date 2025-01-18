bc3af400e9b83ba407cd0111005f9a3575cdc637
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-18 06:25:06 +0000

    changes

---
+++ client/src/pages/Admin/Category.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+export const Category = () => {
+    return (
+        <div>
+            <h2>Category</h2>
+        </div>
+    )
+}
