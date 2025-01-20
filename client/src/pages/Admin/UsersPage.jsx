158296e71ef4b502b1d49a342118df1085affd9e
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-20 04:37:34 +0000

    changes

--- client/src/pages/Admin/UsersPage.jsx
+++ client/src/pages/Admin/UsersPage.jsx
@@ -1,11 +1,24 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 import { FaTrashCan } from "react-icons/fa6";
 import { FiEdit3 } from "react-icons/fi";
 import { IoCheckmarkDoneSharp } from "react-icons/io5";
-
+import {useDeleteUserMutation, useGetUsersQuery, useUpdateUserMutation} from '../../redux/api/UsersApi.js'
 export const UsersPage = () => {
+    const { data: users, refetch, isLoading, error } = useGetUsersQuery();
+
+    const [deleteUser] = useDeleteUserMutation();
+    const [updateUser] = useUpdateUserMutation();
+
+    const [editUserId, setEditUserId] = useState(null);
+    const [editUserName, setEditUserName] = useState('');
+    const [editUserEmail, setEditUserEmail] = useState('');
+
+    useEffect(() => {
+        refetch();
+    }, [refetch])
+
     return (
         <div></div>
     )
