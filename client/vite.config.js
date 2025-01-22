18972d8dfcbaa954cf36a47a04f3b75881dbb7c7
wahomeJohn <johnweru206@gmail.com>
2025-01-22 13:09:16 +0300

    soko

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './', // Ensure the root is correctly set to the client directory
+  build: {
+    outDir: 'dist', // Output directory
+  },
   server: { 
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': 'http://localhost:5000', // Adjust the port to match your backend server
     }
   }
-})
+});
