d598cfe811b699dc338b67803bb1d06ae5773923
wahomeJohn <johnweru206@gmail.com>
2025-01-22 09:28:48 +0300

    progress

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
