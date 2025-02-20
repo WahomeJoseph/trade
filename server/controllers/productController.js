f7d660ff2cccdb2df273cba722716fc5a765e89c
wahomeJohn <johnweru206@gmail.com>
2025-02-20 14:04:08 +0300

    progress changes!

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -71,7 +71,7 @@ export const fetchProducts = async (req, res) => {
   try {
     const pageSize = 3;
     const keyword = req.query.keyword
-     ? {
+      ? {
         name: {
           $regex: req.query.keyword,
           $options: "i",
@@ -103,7 +103,7 @@ export const fetchProductById = async (req, res) => {
       res.status(404).json({ message: 'No Product Found!' })
     }
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error!', error: error.message });
+    res.status(500).json({ message: 'Internal Server Error!', error: error.message });
   }
 }
 
@@ -120,17 +120,16 @@ export const fetchAllProducts = async (req, res) => {
   }
 }
 
-// add review to a product
+// add rating & review to a product
 export const addProductReview = async (req, res) => {
   try {
     const { rating, comment } = req.body;
     const product = await Product.findById(req.params.id);
 
     if (product) {
-      const alreadyReviewed = product.reviews.find((r) => r.user.toString() === req.user._id.toString());
-
+      const alreadyReviewed = product.reviews.find((rev) => rev.user.toString() === req.user._id.toString());
       if (alreadyReviewed) {
-        res.status(400).json({ message: 'Product Already Reviewed!' });
+        res.status(40).json({ message: 'Product Already Reviewed!' });
       }
 
       const review = {
@@ -143,28 +142,31 @@ export const addProductReview = async (req, res) => {
       product.reviews.push(review);
       product.numReviews = product.reviews.length;
 
-      product.rating =
-        product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) /
         product.reviews.length;
 
       await product.save();
       res.status(200).json({ message: 'Product Review Added!' });
     } else {
-      res.status(404).json({ message: 'Product Review not Added!' })
+      res.status(400).json({ message: 'Product Review not Added!' })
     }
   } catch (error) {
-    res.status(500).json({message: 'Internal Server Error', error:error.message});
+    res.status(500).json({ message: 'Internal Server Error', error: error.message });
   }
 }
 
 // filter by getting top products based on the rating and reviews
 export const fetchTopProducts = async (req, res) => {
   try {
-    const products = await Product.find({}).sort({ rating: -1 }).limit(4);
-    res.json(products);
+    const products = await Product.find({}).sort({ rating: -1, reviews: -1 }).limit(3)
+
+    if (!products || products.length === 0) {
+      res.status(404).json({ message: 'No Top-rated Products Found!' });
+    }
+
+    res.status(200).json({ message: 'Top Product Sales!', products });
   } catch (error) {
-    console.error(error);
-    res.status(400).json(error.message);
+    res.status(500).json({ message: 'Internal Server Error!', error: error.message });
   }
 }
 
@@ -172,14 +174,13 @@ export const fetchTopProducts = async (req, res) => {
 export const fetchNewProducts = async (req, res) => {
   try {
     const products = await Product.find().sort({ _id: -1 }).limit(5);
-    res.json(products);
+    res.status(200).json({ message: 'Newest Products!', products });
   } catch (error) {
-    console.error(error);
-    res.status(400).json(error.message);
+    res.status(500).json({ message: 'Internal Server Error!', error: error.message });
   }
 }
 
-// filter products based on price
+// filter products based on price choice
 export const filterProducts = async (req, res) => {
   try {
     const { checked, radio } = req.body;
@@ -189,9 +190,8 @@ export const filterProducts = async (req, res) => {
     if (radio.length) args.price = { $gte: radio[0], $lte: radio[1] };
 
     const products = await Product.find(args);
-    res.json(products);
+    res.status(200).json({ message: 'Filtered Products', products });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Server Error" });
+    res.status(500).json({ message: 'Internal Server Error!', error: error.message });
   }
 }
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -12,10 +12,10 @@ productRouter.get('/',authUsers,authAdmin, fetchProducts)
 productRouter.get('/all-products', authUsers, authAdmin, fetchAllProducts)
 productRouter.post('/:id/reviews', authUsers, addProductReview)
 
+productRouter.post('/filter', filterProducts)
+productRouter.get('/top', fetchTopProducts)
+productRouter.get('/new', fetchNewProducts)
+
 productRouter.get('/:id', fetchProductById)
 productRouter.put('/:id', authUsers, authAdmin, updateProduct)
 productRouter.delete('/:id', authUsers, authAdmin, removeProduct)
-
-productRouter.get('/top-products', fetchTopProducts)
-productRouter.get('/new-products',  fetchNewProducts,)
-productRouter.post('/filter-products', filterProducts)
